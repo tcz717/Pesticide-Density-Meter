2f94163d9d9c1f0ad9b253ba0c8cd4b3d139535a
tcz717 <tcz717@hotmail.com>
2016-04-24 18:37:45 +0800

    实现握手部分
    Signed-off-by: tcz717 <tcz717@hotmail.com>

--- applications/zigbee_task.c
+++ applications/zigbee_task.c
@@ -1,44 +1,145 @@
-#include <board.h>
-#include <rtthread.h>
-#include <components.h>
+#include "zigbee_task.h"
 
-#define ZIGBEE_TICK			5
-#define ZIGBEE_PRIORITY		5
-ALIGN(RT_ALIGN_SIZE)
-static rt_uint8_t zigbee_stack[ 1024 ];
-static struct rt_thread zigbee_thread;
+#define REMOTE_TICK			5
+#define REMOTE_PRIORITY		5
 
+static unsigned char msg_buf[256];
 static rt_device_t uart;
+static struct rt_thread remote_thread;
 static struct rt_semaphore uart_rx_sem;
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t remote_stack[ 1024 ];
+
+unsigned char local_id;
+rt_err_t handshack_handle(struct remote_msg * msg)
+{
+	local_id = msg->content.handshack_re->id;
+	return RT_EOK;
+}
+static const msg_handle handles[REMOTE_CMD_COUNT] =
+{
+	RT_NULL,
+	handshack_handle,
+};
+
+
 static rt_err_t rx_handle(rt_device_t device, rt_size_t size)
 {
-	char ch;
-	while(size--)
+	while(size)
 	{
-		rt_device_read(device,0,&ch,1);
 		rt_sem_release(&uart_rx_sem);
+		size--;
 	}
 	return RT_EOK;
 }
-static void task(void * parameter)
+unsigned char getc()
 {
-	while(rt_sem_take(&uart_rx_sem,RT_WAITING_FOREVER)==RT_EOK)
+	if(rt_sem_take(&uart_rx_sem,RT_WAITING_FOREVER)==RT_EOK)
 	{
+		unsigned char ch;
+		rt_device_read(uart,0,&ch,1);
+		return ch;
+	}
+	return 0;
+}
+rt_err_t readf(unsigned char * buffer,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		buffer[i] = getc();
+	}
+	return RT_EOK;
+}
+
+uint8_t get_sum(unsigned char * data,int size)
+{
+	uint8_t sum=0;
+	for(int i=0;i<size;i++)
+	{
+		sum+=data[i];
+	}
+	return sum;
+}
+static rt_err_t send_msg(struct remote_msg * msg)
+{
+	rt_device_write(uart,0,&msg->head,3);
+	rt_device_write(uart,0,msg->content.raw,msg->head.len);
+	rt_device_write(uart,0,&msg->sum,1);
+	return RT_EOK;
+}
+static rt_err_t receive_msg(struct remote_msg * msg)
+{
+	while(1)
+	{
+		readf((uint8_t*)&msg->head,3);
 		
+		if(!IS_REMOTE_CMD(msg->head.cmd))
+			return RT_ERROR;
+		
+		readf(msg_buf, msg->head.len);
+		msg->content.raw=msg_buf;
+		msg->sum = getc();
+		
+		if(msg->head.id!=local_id)
+			continue;
+		if(msg->sum!=get_sum(msg->content.raw,msg->head.len))
+			return RT_ERROR;
+		
+		return handles[msg->head.cmd](msg);
 	}
 }
-void zigbee_task_init(const char * uart_name)
+void get_chip_id(handshack_t * h)
+{
+	uint32_t * base = (uint32_t *)0x1FFFF7E8;
+	h->id0=*(base++);
+	h->id1=*(base++);
+	h->id2=*(base++);
+}
+static rt_err_t handshack()
+{
+	rt_err_t err;
+	handshack_t h;
+	get_chip_id(&h);
+	
+	struct remote_msg msg=
+	{
+		REMOTE_HANDSHAKE_SIZE,
+		0,
+		REMOTE_HANDSHAKE,
+		(uint8_t *)&h,
+		get_sum((uint8_t *)&h,sizeof(handshack_t)),
+	};
+	send_msg(&msg);
+	
+	err=receive_msg(&msg);
+	return err;
+}
+static void task(void * parameter)
+{
+	struct remote_msg msg;
+	while(handshack()!=RT_EOK)
+	{
+		rt_thread_delay(500);
+	}
+	
+	while(1)
+	{
+		receive_msg(&msg);
+		rt_thread_delay(5);
+	}
+}
+void remote_task_init(const char * uart_name)
 {
 	rt_sem_init(&uart_rx_sem,"uart_rx",0,RT_IPC_FLAG_FIFO);
 	
-	rt_thread_init(&zigbee_thread,
-		"zigbee",
+	rt_thread_init(&remote_thread,
+		"remote",
 		task,
 		RT_NULL,
-		zigbee_stack,
-		sizeof(zigbee_stack),
-		ZIGBEE_PRIORITY,
-		ZIGBEE_TICK);
+		remote_stack,
+		sizeof(remote_stack),
+		REMOTE_PRIORITY,
+		REMOTE_TICK);
 		
 	uart = rt_device_find(uart_name);
 	rt_device_set_rx_indicate(uart,rx_handle);
--- applications/zigbee_task.h
+++ applications/zigbee_task.h
@@ -12,21 +12,39 @@
 #define REMOTE_GET_VALUE			4
 #define REMOTE_GET_VALUE_RESPONSE	5
 #define REMOTE_SET_PARAM			6
-#define REMOTE_CLOSE				0xff
+#define REMOTE_CLOSE				7
 
-#define IS_REMOTE_CMD(C) ((C) <= REMOTE_SET_PARAM || (C) == REMOTE_CLOSE)
+#define REMOTE_HANDSHAKE_SIZE		12
+#define REMOTE_CMD_COUNT			8
+#define IS_REMOTE_CMD(C) ((C) <= REMOTE_CLOSE)
 
+typedef struct 
+{
+	uint32_t id0;
+	uint32_t id1;
+	uint32_t id2;
+} handshack_t; 
 struct remote_msg
 {
-	uint8_t len;
-	uint8_t id;
-	uint8_t cmd;
+	struct
+	{
+		uint8_t len;
+		uint8_t id;
+		uint8_t cmd;
+	}head;
 	union 
 	{
 		uint8_t * raw;
+		handshack_t *handshack;
+		struct 
+		{
+			uint8_t id;
+		} *handshack_re;
 	} content;
 	uint8_t sum;
 };
+
+typedef rt_err_t (*msg_handle)(struct remote_msg *);
 
 extern unsigned char local_id;
 void remote_task_init(const char *);
